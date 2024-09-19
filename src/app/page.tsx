bc9a36a8f53b54db31d9ad10a800540b5e7bf160
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-09-19 20:55:17 -0300

    deploy dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      TESTANDO DELOY DEV GITHUB ACTIONS
+      TESTANDO DELOY DEV GITHUB ACTIONS DEV - 2222222222222222222222
     </div>
   );
 }
